4d7e3642aa59dd05436b386e6632b000ba81eb15
Ibrahim Chehab (IbraTech04) <chehab.ibrahim@outlook.com>
2025-05-10 22:38:08 -0400

    Added proper FXML so IntelliJ stops complaining

--- src/main/java/com/ibrasoft/jdriveclonr/ui/WelcomeController.java
+++ src/main/java/com/ibrasoft/jdriveclonr/ui/WelcomeController.java
@@ -3,8 +3,12 @@ package com.ibrasoft.jdriveclonr.ui;
 import com.ibrasoft.jdriveclonr.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 
 public class WelcomeController {
+    @FXML
+    public ImageView logoImageView;
+
     @FXML
     protected void onStartClicked(ActionEvent event) {
         App.navigateTo("auth.fxml");
