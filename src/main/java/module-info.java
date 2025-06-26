1e38a49e5ab268b0606600188989f01eb9b5857e
Ibrahim Chehab (IbraTech04) <chehab.ibrahim@outlook.com>
2025-06-26 19:08:52 -0400

    [REF] jDriveClonr-2: Cleanup module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,15 @@
 module com.ibrasoft.jdriveclonr {
-    // JavaFX modules
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
     requires javafx.graphics;
 
     requires jdk.httpserver;
 
-    // Google API Client Libraries
-    requires google.api.client; // Changed from 'requires' to 'requires transitive'
+    requires google.api.client;
     requires com.google.gson;
     requires com.google.common;
     requires com.fasterxml.jackson.core;
     requires com.google.api.client.auth;
-//    requires com.google.api.services.drive;
     requires com.google.api.client.json.gson;
     requires com.google.api.client.extensions.java6.auth;
     requires com.google.api.client.extensions.jetty.auth;
@@ -24,17 +21,14 @@ module com.ibrasoft.jdriveclonr {
     requires google.photos.library.client;
     requires com.google.api.client;
     requires org.slf4j;
-//    requires google.api.services.sheets.v4.rev612;
     requires google.api.services.slides.v1.rev399;
     requires google.api.services.drive.v3.rev197;
     requires google.api.services.sheets.v4.rev614;
 
-    // Allow FXML loader to access controllers
     opens com.ibrasoft.jdriveclonr to javafx.fxml;
     opens com.ibrasoft.jdriveclonr.ui to javafx.fxml;
     opens com.ibrasoft.jdriveclonr.auth to javafx.fxml;
     
-    // Export packages for use by other modules
     exports com.ibrasoft.jdriveclonr;
     exports com.ibrasoft.jdriveclonr.ui;
     exports com.ibrasoft.jdriveclonr.auth;
